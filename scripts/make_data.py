019bd97dd44907155b2152a0a109282339c77444
Lisa McBride <lisaleemcb@gmail.com>
2025-12-03 10:11:32 +0100

    fixed path error in script

--- scripts/make_data.py
+++ scripts/make_data.py
@@ -22,68 +22,71 @@ print("=====================================")
 fwhm = np.deg2rad(20.0 / 60)  # to convert arcmin to degrees
 redshift_range = [2.4, 3.4]
 
-halo_filename = f"{dir_rewrite}/websky_halos-lesslight_20230612.h5"
-halos = maps.make_halo_catalogue(halo_filename, verbose=True)
+# halo_filename = f"{dir_rewrite}/websky_halos-lesslight_20230612.h5"
+# halos = maps.make_halo_catalogue(halo_filename, verbose=True)
 
-print(f"making full halo patch...")
-map_halo = maps.make_sky(
-    halos, weights=halos["M200m"] ** (5.0 / 3.0), fwhm=fwhm, verbose=True
-)
+# print()
+# print(f"making full halo patch...")
+# map_halo = maps.make_sky(
+#     halos, weights=halos["M200m"] ** (5.0 / 3.0), fwhm=fwhm, verbose=True
+# )
 
-patch_halo = maps.zoom_in(map_halo)
+# patch_halo = maps.zoom_in(map_halo)
 
-np.save(f"{dir_save}/patch_halo", patch_halo)
-print("saved patch_halo!")
+# np.save(f"{dir_save}/patch_halo", patch_halo)
+# print("saved patch_halo!")
+# print()
+# print(f"making redshift-masked halo patch in range {redshift_range}...")
 
-print(f"making redshift-masked halo patch in range {redshift_range}...")
+# redshift_mask = (halos["redshift"] < max(redshift_range)) & (
+#     halos["redshift"] > min(redshift_range)
+# )
+# print(
+#     f"{np.sum(redshift_mask) / len(halos['redshift']) * 100:.3f}% of halos within specified redshift range"
+# )
+# map_halo_masked = maps.make_sky(
+#     halos,
+#     weights=halos["M200m"] ** (5.0 / 3.0),
+#     fwhm=fwhm,
+#     mask=redshift_mask,
+#     verbose=True,
+# )
+# patch_halo_masked = maps.zoom_in(map_halo_masked)
+# np.save(f"{dir_save}/patch_halo_masked", patch_halo_masked)
+# print("saved patch_halo_masked!")
 
-redshift_mask = (halos["redshift"] < max(redshift_range)) & (
-    halos["redshift"] > min(redshift_range)
-)
-print(
-    f"{np.sum(redshift_mask) / len(halos['redshift']) * 100:.3f}% of halos within specified redshift range"
-)
-map_halo_masked = maps.make_sky(
-    halos,
-    weights=halos["M200m"] ** (5.0 / 3.0),
-    fwhm=fwhm,
-    mask=redshift_mask,
-    verbose=True,
-)
-patch_halo_masked = maps.zoom_in(map_halo_masked)
-np.save(f"{dir_save}/patch_halo_masked", patch_halo_masked)
-print("saved patch_halo_masked!")
+# print()
 
-print()
+# print("Now loading tSZ signal...")
+# tsz = hp.read_map(f"{dir_websky}/websky/0.4/tsz_8192_hp.fits", h=False)
+# tsz = hp.smoothing(tsz, fwhm=fwhm)
+# patch_tsz = maps.zoom_in(tsz)
 
-print("Now loading on tSZ signal...")
-tsz = hp.read_map(f"{dir_websky}/websky/0.4/tsz_8192_hp.fits", h=False)
-tsz = hp.smoothing(tsz, fwhm=fwhm)
-patch_tsz = maps.zoom_in(tsz)
+# np.save(f"{dir_save}/patch_tsz", patch_tsz)
 
-np.save(f"{dir_save}/patch_tsz", patch_tsz)
+# print('tSZ patch saved!')
+# print()
+# print("Now loading CIB signal...")
+# path_cib = "/data/cluster/emc-brid/Datasets/Websky/websky/v0.0"
+# suffix = "cib_nu"
+# # cib_freqs = ["0093", "0143", "0217"]
 
-print("Now loading CIB signal...")
-path_cib = "/data/cluster/emc-brid/Datasets/Websky/websky/v0.0"
-suffix = "cib_nu"
-# cib_freqs = ["0093", "0143", "0217"]
+# cib_freqs = ["0100", "0143", "0145", "0217", "0278", "0353", "0545", "0857"]
 
-cib_freqs = ["0100", "0143", "0145", "0217", "0278", "0353", "0545", "0857"]
+# data_cib = []
 
-data_cib = []
+# for freq in cib_freqs:
+#     print(f"making cib data for freq {freq}...")
+#     m = hp.read_map(f"{path_cib}/{suffix}{freq}.fits")
+#     p = maps.zoom_in(hp.smoothing(m, fwhm=fwhm))
 
-for freq in cib_freqs:
-    print(f"making cib data for freq {freq}...")
-    m = hp.read_map(f"{path_cib}/{suffix}{freq}.fits")
-    p = maps.zoom_in(hp.smoothing(m, fwhm=fwhm))
+#     data_cib.append(p)
 
-    data_cib.append(p)
+# data_cib = np.asarray(data_cib)
 
-data_cib = np.asarray(data_cib)
-
-print("Saving data_cib...")
-np.save(f"{dir_save}/data_cib", data_cib)
-
+# print("Saving data_cib...")
+# np.save(f"{dir_save}/data_cib", data_cib)
+# print()
 print("Now loading CO signal...")
 
 halo_fn_co = f"{dir_co}/websky_halos-light.hdf5"
@@ -104,7 +107,7 @@ CO_fluxes = []
 for freq in CO_obs_freqs:
     CO_chunks = []
     for chunk in [1, 2, 3, 4]:
-        filename = f"{dir_co}/cen_chunk{chunk}_fluxCO_{freq}.h5"
+        filename = f"{dir_co}/sources/cen_chunk{chunk}_fluxCO_{freq}.h5"
         print(filename)
         with h5py.File(filename, "r") as f:
             # List all groups (like folders in the file)
